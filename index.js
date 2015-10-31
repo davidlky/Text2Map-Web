d13fb29c25cc596317e12435fa5261cc6faff587
Li Xuanji <xuanji@gmail.com>
2015-11-01 04:32:56 +0800

    port

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ app.get('/health', function (req, res) {
 });
 
 
-var server = app.listen(3000, function () {
+var port = +process.argv[2] || 4000;
+
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
