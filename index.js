6e7400193bde0aa17cd0f8e4ee3f6550f0da638e
Li Xuanji <xuanji@gmail.com>
2015-11-01 03:42:12 +0800

    add stub for route endpoint

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
-app.get('/', function (req, res) {
-    var a = "Xuanji";
-    res.send(a);
-});
+try {
+  fs.accessSync('GOOGLE_MAPS_API_KEY');
+} catch (e) {
+  console.log('Please create GOOGLE_MAPS_API_KEY file in current directory');
+  process.exit();
+}
 
+var GOOGLE_MAPS_API_KEY = fs.readFileSync('GOOGLE_MAPS_API_KEY', 'utf-8').slice(0, -1);
+var GOOGLE_MAPS_ENDPOINT = "https://maps.googleapis.com/maps/api/directions/json";
+
+app.get('/route/from/:lat/:long/to/:loc', function (req, res) {
+  var from_lat = req.params.lat;
+  var from_lng = req.params.lng;
+  var to_loc = req.params.loc
+  res.json({
+    'lat': lat,
+  });
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
